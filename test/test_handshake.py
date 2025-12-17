0b9146c3c29a75bcf32e1f254fdf5b88284d01e6
Benoit Martin <bmartin@cea.fr>
2025-12-17 11:48:37 +0100

    add multiple bridges in handshake test + fix passing failed tests

--- test/test_handshake.py
+++ test/test_handshake.py
@@ -1,5 +1,6 @@
 import time
 from multiprocessing import Process
+from typing import List
 
 import pytest
 from distributed import LocalCluster, Client
@@ -18,55 +19,61 @@ def env_setup():
     cluster.close()
 
 
-def start_deisa_handshake(address: str):
+def start_deisa_handshake(address: str, nb_bridge: int):
     client = get_connection_info(address)
     handshake = Handshake('deisa', client)
-    assert handshake.get_nb_bridges() == 1
+    assert handshake.get_nb_bridges() == nb_bridge
     assert handshake.get_arrays_metadata() == {'hello': 'world'}
 
 
-def start_bridge_handshake(address: str):
+def start_bridge_handshake(address: str, id: int, max: int):
     client = get_connection_info(address)
-    handshake = Handshake('bridge', client, id=0, max=1, arrays_metadata={'hello': 'world'})
+    handshake = Handshake('bridge', client, id=id, max=max, arrays_metadata={'hello': 'world'})
 
 
-def test_handshake_deisa_first(env_setup):
+@pytest.mark.parametrize('nb_bridge', [1, 4, 64])
+def test_handshake_deisa_first(env_setup, nb_bridge: int):
     client, cluster = env_setup
     addr = cluster.scheduler.address
     print(f"cluster={cluster}, addr={addr}", flush=True)
 
-    processes = []
+    processes: List[Process] = []
 
-    p = Process(target=start_deisa_handshake, args=(addr,))
+    p = Process(target=start_deisa_handshake, args=(addr, nb_bridge))
     processes.append(p)
     p.start()
 
     time.sleep(1)
 
-    p = Process(target=start_bridge_handshake, args=(addr,))
-    processes.append(p)
-    p.start()
+    for i in range(nb_bridge):
+        p = Process(target=start_bridge_handshake, args=(addr, i, nb_bridge))
+        processes.append(p)
+        p.start()
 
     for p in processes:
         p.join()
+        assert p.exitcode == 0
 
 
-def test_handshake_bridge_first(env_setup):
+@pytest.mark.parametrize('nb_bridge', [1, 4, 64])
+def test_handshake_bridge_first(env_setup, nb_bridge: int):
     client, cluster = env_setup
     addr = cluster.scheduler.address
     print(f"cluster={cluster}, addr={addr}", flush=True)
 
-    processes = []
+    processes: List[Process] = []
 
-    p = Process(target=start_bridge_handshake, args=(addr,))
-    processes.append(p)
-    p.start()
+    for i in range(nb_bridge):
+        p = Process(target=start_bridge_handshake, args=(addr, i, nb_bridge))
+        processes.append(p)
+        p.start()
 
     time.sleep(1)
 
-    p = Process(target=start_deisa_handshake, args=(addr,))
+    p = Process(target=start_deisa_handshake, args=(addr, nb_bridge))
     processes.append(p)
     p.start()
 
     for p in processes:
         p.join()
+        assert p.exitcode == 0
